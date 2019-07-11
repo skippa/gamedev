1eca46e10fc628b2107abbd197daaa714aac1348
skippa <cmskipsey@gmail.com>
2019-07-10 21:43:45 -0500

    FIxed sound play issue
    thanks r/C_Programming

--- bouncing_ball.c
+++ bouncing_ball.c
@@ -1,6 +1,7 @@
 #include <simple2d.h>
 #include <stdio.h>
 
+//constants to use for ball direction and boolean
 #define LEFT -1
 #define RIGHT 1
 #define UP -1
@@ -11,10 +12,11 @@
 const int screen_width = 640;
 const int screen_height = 480;
 const int ball_r = 30;
-const int speed = 10;
+const int speed = 5;
 int ball_y, ball_x = 0;
 int dir_y, dir_x = 1;
 bool hit_x,hit_y = FALSE;
+S2D_Sound *snd = NULL;
 
 void update() {
     if(ball_x == (screen_width-ball_r)){ //hit right side
@@ -42,18 +44,17 @@ void render() {
     S2D_DrawCircle(ball_x, ball_y, ball_r, 50, 1, 0, 0, 1);
 
     if(hit_x||hit_y){
-        //S2D_PlaySound(snd);//play bounce sound
+        S2D_PlaySound(snd);//play bounce sound
     }
 }
 
 int main() {
-    ball_x, ball_y = ball_r; //set ball start point
-    S2D_Sound * snd = S2D_CreateSound("bounce.wav"); //initialise sound
+    ball_x = ball_r; //set ball start x point
+    ball_y = ball_r; //set ball start y point
+    snd = S2D_CreateSound("bounce.wav"); //initialise sound
     S2D_Window *window = S2D_CreateWindow(
         "Hello Triangle", screen_width, screen_height, update, render, 0);//init the window
-    S2D_FreeSound(snd);//free sound from mem
     S2D_Show(window);//show the window
+    S2D_FreeSound(snd); //free sound from mem
   return 0;
 }
-
-
